49e5b981028d522b714f0e5b514e6392e6f79dfb
GOD <alvinpoudels@gmail.com>
2021-05-15 10:41:44 -0500

    cam effect

---
+++ app/src/main/java/com/example/arframe/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.arframe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import android.widget.Toast;
+import org.opencv.*;
+import org.opencv.imgproc.Imgproc;
+
+import android.util.Log;
+import android.view.SurfaceView;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+
+    CameraBridgeViewBase cameraBridgeViewBase;
+    BaseLoaderCallback baseLoaderCallback;
+    int counter = 0;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cameraBridgeViewBase = (JavaCameraView) findViewById(R.id.CameraView);
+        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        cameraBridgeViewBase.setCvCameraViewListener(this);
+
+        baseLoaderCallback = new BaseLoaderCallback( this){
+            @Override
+            public void onManagerConnected ( int status){
+                super.onManagerConnected(status);
+                    switch (status) {
+                        case BaseLoaderCallback.SUCCESS:
+                            cameraBridgeViewBase.enableView();
+                            break;
+                        default:
+                            super.onManagerConnected(status);
+                            break;
+
+                    }
+                }
+            } ;
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        Mat frame = inputFrame.rgba();
+
+//        if (counter %2 == 0)
+//        {
+//            Core.flip(frame, frame, 1);
+//            Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2GRAY);
+//
+//        }
+//        counter = counter + 1;
+
+
+
+
+
+
+
+
+        return frame;
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (!OpenCVLoader.initDebug()){
+
+            Toast.makeText(getApplicationContext(),"There's a problem, yo!", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            baseLoaderCallback.onManagerConnected(baseLoaderCallback.SUCCESS);
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if(cameraBridgeViewBase!=null) {
+            cameraBridgeViewBase.disableView();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if(cameraBridgeViewBase!=null) {
+            cameraBridgeViewBase.disableView();
+        }
+    }
+
+
+}
